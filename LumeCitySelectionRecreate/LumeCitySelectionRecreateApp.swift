85ad02881e34660cca1e22cd869cd707534b4047
Yossa Bourne <yossabourne@Terminox-MBP.local>
2025-04-16 16:30:48 +0700

    chore: first commit

---
+++ LumeCitySelectionRecreate/LumeCitySelectionRecreateApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumeCitySelectionRecreateApp.swift
+//  LumeCitySelectionRecreate
+//
+//  Created by Yossa Bourne on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct LumeCitySelectionRecreateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
